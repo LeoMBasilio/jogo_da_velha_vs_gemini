85884842aad6766cb620937a949d58e732d1fb17
LeoMBasilio <lmb.basilio@gmail.com>
2025-01-14 09:42:17 -0300

    Implementação da lógica do jogo da velha para modo multiplayer e IA

--- Models/Gemini.py
+++ Models/Gemini.py
@@ -1,39 +1,47 @@
 import os
 import google.generativeai as genai
 
-genai.configure(api_key=os.environ["GEMINI_API_KEY"])
 
-# Create the model
-generation_config = {
-  "temperature": 1,
-  "top_p": 0.95,
-  "top_k": 40,
-  "max_output_tokens": 8192,
-  "response_mime_type": "text/plain",
-}
+class Gemini:
+    def __init__(self):
+        genai.configure(api_key=os.environ["GEMINI_API_KEY"])
+        generation_config = {
+            "temperature": 1,
+            "top_p": 0.95,
+            "top_k": 40,
+            "max_output_tokens": 8192,
+            "response_mime_type": "text/plain",
+        }
+        self.model = genai.GenerativeModel(
+            model_name="gemini-2.0-flash-exp", generation_config=generation_config
+        )
 
-model = genai.GenerativeModel(
-  model_name="gemini-2.0-flash-exp",
-  generation_config=generation_config,
-)
+    @classmethod
+    def start_chat(self):
+        chat_session = self.model.start_chat(
+            history=[
+                {
+                    "role": "user",
+                    "parts": [
+                        "sera informado a voce a coordenada jogada em um jogo da velha e quero que me retorne somente a coordenada da sua jogada",
+                    ],
+                },
+                {
+                    "role": "model",
+                    "parts": [
+                        "Com certeza! Pode me informar a coordenada da sua jogada e eu retornarei a minha. 😉\n",
+                    ],
+                },
+            ]
+        )
+        return chat_session
 
-chat_session = model.start_chat(
-  history=[
-    {
-      "role": "user",
-      "parts": [
-        "sera informado a voce a coordenada jogada em um jogo da velha e quero que me retorne somente a coordenada da sua jogada",
-      ],
-    },
-    {
-      "role": "model",
-      "parts": [
-        "Com certeza! Pode me informar a coordenada da sua jogada e eu retornarei a minha. 😉\n",
-      ],
-    },
-  ]
-)
+    @classmethod
+    def send_message(self, chat_session, message):
+        response = chat_session.send_message(message)
+        return response
 
-response = chat_session.send_message("INSERT_INPUT_HERE")
-
-print(response.text)
+    def get_response(self, message):
+        chat_session = self.start_chat()
+        response = self.send_message(chat_session, message)
+        return response
--- app.py
+++ app.py
@@ -1,24 +1,52 @@
 from Controllers import Multplayer, Multplayer_Ai
 from asyncio import sleep
 
-print("Welcome to ")
-print("1. Multiplayer")
-print("2. Multiplayer AI")
-print("0. Exit")
 while True:
-    try:
-        match int(input()):
-            case 1:
-                Multplayer.multPlayer()
-                break
-            case 2:
-                Multplayer_Ai.multplayer_AI()
-                break
-            case 0:
-                exit()
-            case _:
-                print("Invalid option")
-                continue
-    except ValueError:
-        print("Invalid option")
-        continue
+    print(fr'''  
+     ___                         _         _   _      _ _           
+    |_  |                       | |       | | | |    | | |          
+      | | ___   __ _  ___     __| | __ _  | | | | ___| | |__   __ _ 
+      | |/ _ \ / _` |/ _ \   / _` |/ _` | | | | |/ _ \ | '_ \ / _` |
+  /\__/ / (_) | (_| | (_) | | (_| | (_| | \ \_/ /  __/ | | | | (_| |
+  \____/ \___/ \__, |\___/   \__,_|\__,_|  \___/ \___|_|_| |_|\__,_|
+                __/ |                                               
+               |___/                                                ''')
+
+    print("Welcome to ")
+    print("1. Multiplayer")
+    print("2. Multiplayer AI")
+    print("0. Exit")
+    while True:
+        try:
+            match int(input()):
+                case 1:
+                    Multplayer.multPlayer()
+                    break
+                case 2:
+                    Multplayer_Ai.multplayer_Ai()
+                    break
+                case 0:
+                    exit()
+                case _:
+                    print("Invalid option")
+                    continue
+        except ValueError:
+            print("Invalid option")
+            continue
+    
+    print("Do you want to play again?")
+    print("1. Yes")
+    print("0. No")
+    while True:
+        try:
+            match int(input()):
+                case 1:
+                    break
+                case 0:
+                    exit()
+                case _:
+                    print("Invalid option")
+                    continue
+        except ValueError:
+            print("Invalid option")
+            continue
